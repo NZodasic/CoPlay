2a5aba911b8bd707df52bb050619a4405a1d5a5c
Nguyễn Võ Chí Dũng <23560012@gm.uit.edu.vn>
2024-01-02 20:20:43 +0700

    Upload

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a = 3, b = 12;
+
+    printf("Kq1 = %d \t", a/b);
+
+    printf("Kq2 = %d \t", (double)a/b);
+
+    printf("Kq3 = %d \t", (double)(a/b));
+
+
+    return 0;
+}
